df4450958d12f40f66310c0a7a1ebaf71036734b
ABChocha <achochadswt@gmail.com>
2024-01-20 23:53:33 +0530

    connect to the database

--- api/index.js
+++ api/index.js
@@ -1,6 +1,11 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 const app = express();
+dotenv.config();
+mongoose.connect(process.env.MONGOOSE)
+    .then(() => { console.log('mongoDB is connectd') })
 
 const server = app.listen(5000, () => {
     console.log(`server start at port ${server.address().port}`)
-})
+}) 
