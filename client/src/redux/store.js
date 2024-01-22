ecfbc923dd89052a534ebc7ec99ddfe67a3262aa
ABchocha17 <achochadswt@gmail.com>
2024-01-22 16:13:03 +0530

    redux data store in browser

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,9 +1,27 @@
-import { configureStore } from '@reduxjs/toolkit'
-import userReducer from './user/userSlice'
+import { configureStore, combineReducers } from '@reduxjs/toolkit';
+import userReducer from './user/userSlice';
+import { persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage'; // Import storage from redux-persist
+import persistStore from 'redux-persist/es/persistStore';
+
+const rootReducer = combineReducers({
+  user: userReducer,
+});
+
+const persistConfig = {
+  key: 'root',
+  storage, // Fix: Use storage instead of Storage
+  version: 1,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
-  reducer: {
-    user: userReducer,
-  },
-})
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: false,
+    }),
+});
 
+export const persistor = persistStore(store);
