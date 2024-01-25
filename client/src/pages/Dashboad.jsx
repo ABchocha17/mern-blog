adadbd2d6dfe24201fd96d8f67f09769367d2e10
ABchocha17 <achochadswt@gmail.com>
2024-01-25 15:07:48 +0530

    complet dashboad sidebar

--- client/src/pages/Dashboad.jsx
+++ client/src/pages/Dashboad.jsx
@@ -1,6 +1,28 @@
+import { useEffect, useState } from 'react';
+import {useLocation} from 'react-router-dom'
+import DashSidebar from '../components/DashSidebar';
+import DashProfile from '../components/DashProfile';
 
 export default function Dashboad() {
+    const location = useLocation();
+    const [tab,setTab] = useState('');
+    useEffect(()=>{
+        const urlParams = new URLSearchParams(location.search);
+        const tabFromUrl = urlParams.get('tab');
+        if(tabFromUrl){
+            setTab(tabFromUrl);
+        }
+    },[location.search])
     return (
-        <div>Dashboad</div>
+        <div className='flex flex-col md:flex-row min-h-[69vh]'>
+            {/* side bar */}
+            <div className="">
+                <DashSidebar />
+            </div>
+            {/* profile */}
+            <div className="p-10">
+                {tab === 'profile' && <DashProfile />}
+            </div>
+        </div>  
     )
 }
